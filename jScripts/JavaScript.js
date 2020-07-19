9876ddac80c0982f59bdab4ffa5dd017f9947ff6
maypoleg <64608406+maypoleg@users.noreply.github.com>
2020-07-19 17:08:30 +0300

    Add files via upload

---
+++ jScripts/JavaScript.js
@@ -0,0 +1,65 @@
+﻿// ===== Scroll to Top ==== 
+$(window).scroll(function () {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function () {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop: 0                       // Scroll to top of body
+    }, 500);
+});
+
+
+var slideIndex1 = 1;
+var slideIndex2 = 1;
+showSlides(slideIndex2);
+function plusSlides(n) {
+    showSlides(slideIndex2 += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex2 = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides2");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex2 = 1 }
+    if (n < 1) { slideIndex2 = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex2 - 1].style.display = "block";
+    dots[slideIndex2 - 1].className += " active";
+}
+
+
+
+show(slideIndex1);
+function plus(n) {
+    show(slideIndex1 += n);
+
+}
+function current(n) {
+    show(slideIndex1 = n);
+}
+function show(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides1");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex1 = 1 }
+    if (n < 1) { slideIndex1 = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex1 - 1].style.display = "block";
+    dots[slideIndex1 - 1].className += " active";
+}
